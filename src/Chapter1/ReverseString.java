54d054e9e9351cc78ed8b2d4e1e232baefe7b051
Joao Fernandes <joaofernandes@jff-macbook.local>
2013-11-04 21:08:08 +0000

    Improved code and tests for reversing a string

--- src/Chapter1/ReverseString.java
+++ src/Chapter1/ReverseString.java
@@ -3,29 +3,32 @@ package Chapter1;
 import java.util.Stack;
 
 public class ReverseString {
-	public static final int MAX_SIZE = 1000;
-	private char[] s;
+	public static final int MAX_SIZE = 100;
+	public static final char END_OF_STRING = '\0';
+	private char[] s; //this could be final
+	private char[] reversed;
+	private int size;
 	
 	public ReverseString (char[] s) {
 		this.s = s;
+		Stack<Character> stack = new Stack<Character>();
+		int i = 0;
+		while(s[i] != END_OF_STRING && i < MAX_SIZE) {
+			stack.add(s[i++]);
+		}
+		size = i + 1;
+		reversed = new char[size];
+		i = 0;
+		while(!stack.isEmpty()) {
+			reversed[i++] = stack.pop();
+		}
+		reversed[i] = END_OF_STRING;
 	}
 	
 	public char[] Reverse() {
-		Stack<Character> stack = new Stack<Character>();
-		int i = 0;
-		while(s[i] != '\n' && i < MAX_SIZE) {
-			stack.add(s[i++]);
-		}
-		char[] str = new char[i+1];
-		i = 0;
-		while(!stack.isEmpty()) {
-			str[i++] = stack.pop();
-		}
-		stack.add('\n');
-		
-		return str;
+		return this.reversed;
 	}
-	
+
 	public static int CompareTo(char[] c1, char[] c2) {
 		if(c1 == c2) {
 			return 0;
@@ -41,21 +44,39 @@ public class ReverseString {
 			return 1;
 		}
 		int i = 0;
-		while (c1[i] != '\n' && c2[i] != '\n') {
+		while (c1[i] != END_OF_STRING && c2[i] != END_OF_STRING) {
 			if (c1[i] < c2[i]) {
 				return -1;
 			}
 			if (c1[i] > c2[i]) {
 				return 1;
 			}
+			i++;
 		}
-		if (c1[i] == '\n' && c2[i] != '\n') {
+		if (c1[i] == END_OF_STRING && c2[i] != END_OF_STRING) {
 			return -1;
 		}
-		if (c1[i] != '\n' && c2[i] == '\n') {
+		if (c1[i] != END_OF_STRING && c2[i] == END_OF_STRING) {
 			return 1;
 		}
 		return 0;
 	}
+	
+	public String PrintString() {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			builder.append(s[i]);
+		}
+		return builder.toString();
+	}
+	
+	public String PrintReverse() {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			builder.append(reversed[i]);
+		}
+		return builder.toString();
+	}
+	
 
 }
