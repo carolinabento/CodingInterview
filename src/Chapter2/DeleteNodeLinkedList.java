4b2d99d4a8a578b02aa0ace53b856a3d788c8ede
carolinabento <carolproductions@gmail.com>
2014-08-11 15:27:16 +0100

    Closes #15

--- src/Chapter2/DeleteNodeLinkedList.java
+++ src/Chapter2/DeleteNodeLinkedList.java
@@ -1,22 +1,25 @@
 package Chapter2;
 
 public class DeleteNodeLinkedList<T> {
-	private Node<T> myNode;
+	private Node<T> node;
+	private LinkedList<T> list;
 	
-	public DeleteNodeLinkedList(Node<T> node){
-		this.myNode = node;
+	public DeleteNodeLinkedList(LinkedList<T> list, Node<T> node){
+		this.node = node;
+		this.list = list;
 	}
 	
 	public void deleteNode(){
 		
-		if(this.myNode == null || this.myNode.isEmpty()){
+		if(this.node == null || this.node.isEmpty()){
 			throw new NullPointerException();
 		}else{
-			
-			if(this.myNode.next != null){
-				this.myNode.value = (T) this.myNode.next.value;
-				this.myNode.next = this.myNode.next.next;				
-			}	
+			if(this.node.next != null){
+				Node<T> temp = this.node.next;
+				this.node.value = (T) this.node.next.value;
+				this.node.next = this.node.next.next;
+				temp = null;
+			}
 		}
 	}
 }
--- src/Chapter2/TestDeleteNodeLinkedList.java
+++ src/Chapter2/TestDeleteNodeLinkedList.java
@@ -1,6 +1,8 @@
 package Chapter2;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.*;
 
 public class TestDeleteNodeLinkedList {
 
@@ -8,44 +10,70 @@ public class TestDeleteNodeLinkedList {
 	@Test(expected = NullPointerException.class)
 	public void emptyTest(){
 		Node<Integer> node = new Node<Integer>();
-		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(node);
+		LinkedList<Integer> myList = new LinkedList<Integer>();
+		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(myList,node);
 		dn.deleteNode();
 	}
 
 	@Test
 	public void deleteTailTest(){
 
-		LinkedList<Integer> myList = new LinkedList<Integer>(new Integer(1));
-		myList.append(new Integer(2));
-		myList.append(new Integer(3));				
-
+		Node<Integer> node1 = new Node<Integer>(new Integer(1));
+		Node<Integer> node2 = new Node<Integer>(new Integer(2));
+		Node<Integer> node3 = new Node<Integer>(new Integer(3));
+		LinkedList<Integer> myList = new LinkedList<Integer>(node1);
+		myList.append(node2.value);
+		myList.append(node3.value);				
+		
 		Node<Integer> node = myList.getHead().next.next;
 
-		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(node);
-		dn.deleteNode();		
+		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(myList,node);
+		dn.deleteNode();	
+
+		assertTrue(myList.getHead().value == node1.value);
+		assertTrue(myList.getHead().next.value == node2.value);
+		assertTrue(myList.getTail().value == node3.value);
 	}
 
 	@Test
 	public void deleteMiddleTest(){
-		LinkedList<Integer> myList = new LinkedList<Integer>(new Integer(1));
+		Node<Integer> node1 = new Node<Integer>(new Integer(1));
+		Node<Integer> node2 = new Node<Integer>(new Integer(2));
+		Node<Integer> node3 = new Node<Integer>(new Integer(3));
+		Node<Integer> node4 = new Node<Integer>(new Integer(4));
+		Node<Integer> node5 = new Node<Integer>(new Integer(5));
+		Node<Integer> node6 = new Node<Integer>(new Integer(6));
+		Node<Integer> node7 = new Node<Integer>(new Integer(7));
+		Node<Integer> node8 = new Node<Integer>(new Integer(8));
+		Node<Integer> node9 = new Node<Integer>(new Integer(9));
+		Node<Integer> node10 = new Node<Integer>(new Integer(10));
+		
+		LinkedList<Integer> myList = new LinkedList<Integer>(node1);
 
-		myList.append(new Integer(2));
-		myList.append(new Integer(3));
-		myList.append(new Integer(4));
-		myList.append(new Integer(5));
-		myList.append(new Integer(6));
-		myList.append(new Integer(7));
-		myList.append(new Integer(8));
-		myList.append(new Integer(9));
-		myList.append(new Integer(10));
-
-		myList.prettyPrint();
-
+		myList.append(node2.value);
+		myList.append(node3.value);
+		myList.append(node4.value);
+		myList.append(node5.value);
+		myList.append(node6.value);
+		myList.append(node7.value);
+		myList.append(node8.value);
+		myList.append(node9.value);
+		myList.append(node10.value);
+		
 		Node<Integer> node = myList.getHead().next.next;
 		
-		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(node);
+		DeleteNodeLinkedList<Integer> dn = new DeleteNodeLinkedList<Integer>(myList,node);
 		dn.deleteNode();
 
-		myList.prettyPrint();
+		
+		assertTrue(myList.getHead().value == node1.value);
+		assertTrue(myList.getHead().next.value == node2.value);
+		assertTrue(myList.getHead().next.next.value == node4.value);
+		assertTrue(myList.getHead().next.next.next.value == node5.value);
+		assertTrue(myList.getHead().next.next.next.next.value == node6.value);
+		assertTrue(myList.getHead().next.next.next.next.next.value == node7.value);
+		assertTrue(myList.getHead().next.next.next.next.next.next.value == node8.value);
+		assertTrue(myList.getHead().next.next.next.next.next.next.next.value == node9.value);
+		assertTrue(myList.getTail().value == node10.value);
 	}
 }
